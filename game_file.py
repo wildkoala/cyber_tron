f0ffe10a2b6e6ecdada92bf02030a3af16ff1454
Twiggy Gordon <31520629+twiggygordon@users.noreply.github.com>
2019-08-01 10:59:26 -0400

    Update game_file.py

--- game_file.py
+++ game_file.py
@@ -1,13 +1,7 @@
 import pygame
+import os
 
 pygame.init()
-
-# Create the screen
-width = 1000
-height = 1000
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("CYBER TRON")
-
 
 # player global variables
 # (x,y) are the coordinates of the top left corner of the player
@@ -17,15 +11,52 @@ player_width = 10
 player_height = 10
 speed = 5
 
+going_left = False
+going_right = False
+going_up = False
+going_down = False
 
+drive_right_img = pygame.image.load(os.path.join('sprites', 'br.png'))
+drive_left_img = pygame.image.load(os.path.join('sprites', 'bl.png'))
+drive_up_img = pygame.image.load(os.path.join('sprites','bu.png'))
+drive_down_img = pygame.image.load(os.path.join('sprites','bd.png'))
+
+
+# Create the screen
+width = 1000
+height = 1000
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("CYBER TRON")
+#spawn_points = [(495,495), (width - player_width, height - player_height)]
+clock = pygame.time.Clock()
 
 # Store the instances of players?
 # players = [Player("Twiggy", Player("Ryan")]
 
 
+def redraw_screen():
+	if going_left:
+		screen.blit(drive_left_img, (x,y))
+		going_left = False
+	if going_right:
+		screen.blit(drive_right_img, (x,y))
+		going_right = False
+	if going_up:
+		screen.blit(drive_up_img, (x,y))
+		going_up = False
+	if going_down:
+		screen.blit(drive_down_img, (x,y))
+		going_down = False
+
+	#pygame.draw.rect(screen, (255, 0 ,255), (x, y, player_width, player_height))
+	pygame.display.update()
+
+
+
 # GAME LOOP
 run = True
 while run:
+
 
 		# Set the screen refresh rate in milliseconds.
 		pygame.time.delay(40)
@@ -39,12 +70,19 @@ while run:
 		keys = pygame.key.get_pressed()
 		if keys[pygame.K_LEFT]:
 			x -= speed
+			going_left = True
+
 		if keys[pygame.K_RIGHT]:
 			x += speed
+			going_right = True
+
 		if keys[pygame.K_UP]:
 			y -= speed
+			going_up = True
+
 		if keys[pygame.K_DOWN]:
 			y += speed
+			going_down = True
 
 		# Handle collisions with the walls - currently causes the game to exit.
 		if x < 0:
@@ -59,11 +97,11 @@ while run:
 		if y > height - player_height:
 			print("COLLIDED WITH LOWER WALL")
 			run = False
-		
-		# Handle collisions with the walls - currently causes the game to exit.
 
-		pygame.draw.rect(screen, (255, 0 ,255), (x, y, player_width, player_height))
-		pygame.display.update()
+		# Handle collisions with the players - GOES HERE
+
+		# Redraw the screen
+		redraw_screen()
 
 pygame.quit()
 
@@ -73,21 +111,17 @@ pygame.quit()
 
 
 
-
 # Classes - NOT YET IN USE
-
-
 class Block:
 	def __init__(self, color, size):
 		self.color = color
 		self.size = size # in terms of pixels
 
+
 class Board:
 	def __init__(self, num_of_players, size):
 		self.num_of_players = num_of_players
 		self.size = size #in terms of blocks
-
-
 
 
 class Player: #give board obj for board.
@@ -100,6 +134,27 @@ class Player: #give board obj for board.
 
 	def dies (self):
 		self.is_alive = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
